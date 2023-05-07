c2ad45b002f81e27b8f1d68d6e202ce91b280601
John Scott <johnrscott0@gmail.com>
2023-05-08 00:24:01 +0100

    Added note about iptables rule.

--- get.py
+++ get.py
@@ -1,14 +1,23 @@
 # "https://stackoverflow.com/questions/4750793/
 #  python-scapy-or-the-like-how-can-i-create-
 #  an-http-get-request-at-the-packet-leve"
+#
+# To stop the kernel from resetting the connection,
+# add this iptables rule:
+#
+# sudo iptables -A OUTPUT -p tcp --tcp-flags RST RST -s 192.168.1.210 -j DROP
+#
+# where the IP is the source IP address of the SYN packet.
 
 from scapy.all import *
 
 syn = IP(dst="192.168.1.190") / TCP(dport=80, flags='S')
+print("SYN packet:")
 syn.show()
 
 # Receive SYN acknowledgement (note the SA flag)
 syn_ack = sr1(syn)
+print("SYN acknowledge:")
 syn_ack.show()
 
 # Now perform the request
