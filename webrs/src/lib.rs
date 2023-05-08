6aa8c011db2ec9efdcc782c156ea0b05f1ca149a
John Scott <johnrscott0@gmail.com>
2023-05-08 16:03:12 +0100

    Tested rust minimal webserver

--- webrs/src/lib.rs
+++ webrs/src/lib.rs
@@ -1,8 +1,11 @@
-use std::{sync::{mpsc, Arc, Mutex} ,thread};
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -18,44 +21,71 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-	let (sender, receiver) = mpsc::channel();
-	
-	let receiver = Arc::new(Mutex::new(receiver));
-	    
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        Self { workers, sender }
+        Self {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
-	let job = Box::new(f);
-	self.sender.send(job).unwrap();
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
         let thread = thread::spawn(move || loop {
-	    let job = receiver
-		.lock()
-		.expect("Panicked when trying to lock rx")
-		.recv()
-		.expect("Error on RX"); 
-	    println!("Worker {id} got a job; executing");
-	    job();
-	});
-        Self { id, thread }
+	    let message = receiver
+                .lock()
+                .expect("Panicked when trying to lock rx")
+                .recv();
+
+	    match message {
+		Ok(job) => { 
+		    println!("Worker {id} got a job; executing.");
+		    job();
+		},
+		Err(_) => {
+		    println!("Worker {id} disconnected; shutting down.");
+		    break;
+		}
+	    }
+        });
+        Self {
+            id,
+            thread: Some(thread),
+        }
     }
 }
